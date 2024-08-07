1a116ca634f94bee43efcefd8be51ce3b9b4b626
Nandan <nandankumar1475@gmail.com>
2024-08-07 13:09:25 +0530

    added custoom button xml

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  function onclickhandler() {
-    setCount(count+1)
-  }
+  // function onclickhandler() {
+  //   setCount(count+1)
+  // }
 
   return (
     <div>
-      <button onClick={onclickhandler}>counter {count}</button>
+      {/* <button onClick={onclickhandler}>counter {count}</button> */}
+
+      <CustomButtom count = {count} setCount= {setCount}></CustomButtom>
     </div>
   )
+  function CustomButtom(props){
+    function onclickhandler(){
+      props.setCount(count+1);
+    }
+    return <button onClick={onclickhandler}>
+      counter {props.count}
+
+    </button>
+
+  }
 }
 
 export default App
